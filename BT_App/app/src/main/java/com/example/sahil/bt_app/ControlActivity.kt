c29f4d9e42dcacf2a8fa7cb0cd895d5ab300983d
Sahil Garg <sahilgarg8901@gmail.com>
2018-03-04 21:19:15 +0530

    basic bluetooth app done

--- BT_App/app/src/main/java/com/example/sahil/bt_app/ControlActivity.kt
+++ BT_App/app/src/main/java/com/example/sahil/bt_app/ControlActivity.kt
@@ -1,12 +1,111 @@
 package com.example.sahil.bt_app
 
+import android.app.ProgressDialog
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
+import android.content.Context
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import java.io.IOException
+import java.util.*
 
 class ControlActivity : AppCompatActivity() {
 
+    companion object {
+        var m_myUUID: UUID = UUID.fromString("  ")// fill the uuid of your bluetooth device
+        var m_bluetoothSocket: BluetoothSocket? = null
+        lateinit var m_progress: ProgressDialog
+        lateinit var m_bluetoothAdapter: BluetoothAdapter
+        var m_isConnected : Boolean = false
+        lateinit var m_address :String
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_control)
+
+        m_address = intent.getStringExtra(MainActivity.EXTRA_ADDRESS)
+
+        ConnectToDevice(this).execute()
+
+
+    }
+
+    private fun onButtonClicked(view: View){
+        sendCommand("a")
+    }
+    private fun offButtonClicked(view: View){
+        sendCommand("b")
+    }
+    private fun disconnectButtonClicked(view: View){
+        disconnect()
+    }
+
+    private fun sendCommand(input: String){
+        if (m_bluetoothSocket!= null){
+            try {
+                m_bluetoothSocket!!.outputStream.write(input.toByteArray())
+            } catch (e: IOException){
+                e.printStackTrace()
+            }
+        }
+    }
+    private fun disconnect() {
+        if (m_bluetoothSocket != null) {
+            try {
+                m_bluetoothSocket!!.close()
+                m_bluetoothSocket = null
+                m_isConnected = false
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+        finish()          // to close the current activity and come back to the previous one
+    }
+
+   // the following is the subClass responsible for Connecting to the bluetooth device
+
+    private class ConnectToDevice(c: Context): AsyncTask<Void, Void, String>(){
+        private var connectSuccess: Boolean = true
+        private val context: Context
+        init {
+            this.context= c
+        }
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+            m_progress = ProgressDialog.show(context,"connecting", "please wait")
+        }
+
+        override fun doInBackground(vararg params: Void?): String?   {
+            try {
+                if (m_bluetoothSocket!= null || !m_isConnected){
+                    m_bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+                    val device : BluetoothDevice = m_bluetoothAdapter.getRemoteDevice(m_address)
+                    m_bluetoothSocket =device.createInsecureRfcommSocketToServiceRecord(m_myUUID)
+                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery()
+                    m_bluetoothSocket!!.connect()
+                }
+            } catch (e: IOException){
+                connectSuccess = false
+                e.printStackTrace()
+            }
+            return null
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            if (!connectSuccess){
+                Log.i("data", "could'nt connect")
+            } else {
+                m_isConnected = true
+            }
+            m_progress.dismiss()
+        }
     }
 }
+
+
